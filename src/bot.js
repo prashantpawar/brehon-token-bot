2c5abe00749a024d5d417c58fb179c94e1e419be
Prashant Singh Pawar <prashantpawar@gmail.com>
2017-06-03 19:00:28 -0400

    - Added SimpleStorage contract and trying to make it work

--- src/bot.js
+++ src/bot.js
@@ -1,8 +1,23 @@
 const Bot = require('./lib/Bot')
 const SOFA = require('sofa-js')
 const Fiat = require('./lib/Fiat')
+const Web3 = require('web3');
+const fs = require("fs");
+const solc = require('solc');
+
+const web3 = new Web3(new Web3.providers.HttpProvider("http://192.168.1.12:8545"));
 
 let bot = new Bot()
+
+// DEPLOY CONTRACT
+const source = fs.readFileSync('./src/contracts/SimpleStorage.sol', 'utf8');
+const compiledContract = solc.compile(source, 1);
+const abi = compiledContract.contracts[':SimpleStorage'].interface;
+const bytecode = compiledContract.contracts[':SimpleStorage'].bytecode;
+const gasEstimate = web3.eth.estimateGas({data: bytecode});
+const SimpleStorage = web3.eth.contract(JSON.parse(abi));
+
+let simpleStorageInstance;
 
 // ROUTING
 
@@ -17,12 +32,6 @@ bot.onEvent = function(session, message) {
     case 'Command':
       onCommand(session, message)
       break
-    case 'Payment':
-      onPayment(session, message)
-      break
-    case 'PaymentRequest':
-      welcome(session)
-      break
   }
 }
 
@@ -32,48 +41,19 @@ function onMessage(session, message) {
 
 function onCommand(session, command) {
   switch (command.content.value) {
-    case 'ping':
-      pong(session)
-      break
     case 'count':
-      count(session)
+      count(session);
       break
-    case 'donate':
-      donate(session)
-      break
+    case 'deploy':
+      deploy(session);
+      break;
     }
-}
-
-function onPayment(session, message) {
-  if (message.fromAddress == session.config.paymentAddress) {
-    // handle payments sent by the bot
-    if (message.status == 'confirmed') {
-      // perform special action once the payment has been confirmed
-      // on the network
-    } else if (message.status == 'error') {
-      // oops, something went wrong with a payment we tried to send!
-    }
-  } else {
-    // handle payments sent to the bot
-    if (message.status == 'unconfirmed') {
-      // payment has been sent to the ethereum network, but is not yet confirmed
-      sendMessage(session, `Thanks for the payment! 🙏`);
-    } else if (message.status == 'confirmed') {
-      // handle when the payment is actually confirmed!
-    } else if (message.status == 'error') {
-      sendMessage(session, `There was an error with your payment!🚫`);
-    }
-  }
 }
 
 // STATES
 
 function welcome(session) {
-  sendMessage(session, `Hello Token!`)
-}
-
-function pong(session) {
-  sendMessage(session, `Pong`)
+  sendMessage(session, `Hello from BrehonBot!`)
 }
 
 // example of how to store state on each user
@@ -83,20 +63,18 @@ function count(session) {
   sendMessage(session, `${count}`)
 }
 
-function donate(session) {
-  // request $1 USD at current exchange rates
-  Fiat.fetch().then((toEth) => {
-    session.requestEth(toEth.USD(1))
-  })
+function deploy(session) {
+  simpleStorageInstance = SimpleStorage.new({data: bytecode, gas: 300000, from: session.user.payment_address});
+  sendMessage(session, simpleStorageInstance.address);
+
 }
 
 // HELPERS
 
 function sendMessage(session, message) {
   let controls = [
-    {type: 'button', label: 'Ping', value: 'ping'},
     {type: 'button', label: 'Count', value: 'count'},
-    {type: 'button', label: 'Donate', value: 'donate'}
+    {type: 'button', label: 'Deploy', value: 'deploy'},
   ]
   session.reply(SOFA.Message({
     body: message,
